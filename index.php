ef7cf4753791b6f49163a8a7e45525319c696689
Leelabari Fulbel <fulbel.leelabari@gmail.com>
2017-02-13 16:36:13 -0600

    changing the calendary

--- index.php
+++ index.php
@@ -31,38 +31,13 @@ echo " <link rel=\"stylesheet\" href=\"css/cssstyle.css\" >";
                 events: [
                     {
                         title: 'All Day Event',
-                        start: '2016-12-01'
+                        start: '2017-2-11'
                     },
                     {
                         title: 'Long Event',
-                        start: '2016-12-07',
-                        end: '2016-12-10'
+                        start: '2017-2-11',
+                        end: '2017-2-19'
                     },
-                    {
-                        id: 999,
-                        title: 'Repeating Event',
-                        start: '2016-12-09T16:00:00'
-                    },
-                    {
-                        id: 999,
-                        title: 'Repeating Event',
-                        start: '2016-12-16T16:00:00'
-                    },
-                    {
-                        title: 'Conference',
-                        start: '2016-12-11',
-                        end: '2016-12-13'
-                    },
-                    {
-                        title: 'Meeting',
-                        start: '2016-12-12T10:30:00',
-                        end: '2016-12-12T12:30:00'
-                    },
-                    {
-                        title: 'Click for Google',
-                        url: 'http://google.com/',
-                        start: '2016-12-28'
-                    }
                 ]
             });
         });
